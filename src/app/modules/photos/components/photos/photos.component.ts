9759e22a51f319a92edd21cea3514ec0c1aabe74
Edwin de los Santos <edwin2delossantos@gmail.com>
2024-10-31 17:30:25 -0400

    refactor: Improve error handling in photos component

--- src/app/modules/photos/components/photos/photos.component.ts
+++ src/app/modules/photos/components/photos/photos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { PhotosService } from '../../services/photos.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-photos',
@@ -9,26 +10,38 @@ import { PhotosService } from '../../services/photos.service';
   styleUrls: ['./photos.component.scss']
 })
 export class PhotosComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'title', 'albumTitle','userEmail', 'photo'];
+  displayedColumns: string[] = ['id', 'title', 'albumTitle', 'userEmail', 'photo'];
   dataSource = new MatTableDataSource<any>([]);
   totalPhotos = 0;
   pageSize = 10;
   pageIndex = 0;
   photoLoadStates: { [key: number]: { loaded: boolean; failed: boolean } } = {};
+  backendLoadFailed = false;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private photosService: PhotosService) {}
+  constructor(private photosService: PhotosService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.loadPhotos();
   }
 
   loadPhotos(): void {
+    this.backendLoadFailed = false;
     const offset = this.pageIndex * this.pageSize;
-    this.photosService.getPhotos(this.pageSize, offset).subscribe(response => {
-      this.dataSource.data = response;
-      this.totalPhotos = 5000;
+    this.photosService.getPhotos(this.pageSize, offset).subscribe({
+      next: (response) => {
+        this.dataSource.data = response;
+        this.totalPhotos = 5000;
+      },
+      error: () => {
+        this.backendLoadFailed = true;
+        this.snackBar.open('Failed to load photos. Please try again.', 'Retry', {
+          duration: 5000
+        }).onAction().subscribe(() => {
+          this.loadPhotos();
+        });
+      }
     });
   }
 
@@ -45,5 +58,4 @@ export class PhotosComponent implements OnInit {
   onPhotoError(photoId: number): void {
     this.photoLoadStates[photoId] = { loaded: false, failed: true };
   }
-
 }
