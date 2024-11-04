ffbc071dde39453d99590ee9fb983c0277146f82
Edwin de los Santos <edwin2delossantos@gmail.com>
2024-11-04 15:23:42 -0400

    refactor: Remove unused code in theme-service.ts

--- src/app/services/theme-service.ts
+++ src/app/services/theme-service.ts
@@ -4,17 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ThemeService {
-  darkMode$: any;
   constructor() {
     this.initTheme();
   }
 
   initTheme(): void {
-    // Check system preference for dark mode
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     this.applyTheme(prefersDark ? 'dark' : 'light');
 
-    // Listen for changes in the system preference
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
       this.applyTheme(event.matches ? 'dark' : 'light');
     });
